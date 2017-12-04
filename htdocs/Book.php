54f2a7700a920f65459384ddcfffc1927b17a4e6
MSIGRENADY <cs.ammarn@gmail.com>
2017-12-03 22:41:13 -0800

    dal
    wrting dal

---
+++ htdocs/Book.php
@@ -0,0 +1,75 @@
+<?php 
+
+require_once ('../lib/RedBeanPHP/rb.php');
+
+
+
+class Book {
+    #rdsf
+    echo 2ll;
+    
+    
+    public function __construct()
+    {
+        if( !R::testConnection()) {
+            R::setup('mysql:host=localhost;dbname=dsr_amnatto',
+                'dsr_amnatto', 'mVNeKCEG]b@W');
+            
+            
+        }
+    }
+    
+    
+    
+    public function save ($ISBN,$title,$author,$publisher,$publication_year){
+        // how to save data 
+    }
+    
+    public function validate_data($ISBN,$title,$author,$publisher,$publication_year){
+        
+    }
+    
+    public function update (){
+        
+    }
+    
+    public function fetchAll(){
+        
+    }
+    
+    public function delete($id)
+    {
+        
+    }
+    
+    public function deleteAll()
+    {
+        
+    }
+    public function fetchWithPK($id)
+    {
+        
+        
+    }
+    
+ 
+    public function print ($book_refrence) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+
+}
+
+
+
+
+
+
+
+?>
