9537a464b1c5a89b2094096fd6df7d75e2a043d5
MSIGRENADY <cs.ammarn@gmail.com>
2017-12-05 12:10:08 -0800

    done with test_save_book_data function
    done with test_save_book_data function

--- unit_test/Book_test.php
+++ unit_test/Book_test.php
@@ -3,15 +3,15 @@
 require_once ('vendor/autoload.php');
 
 require_once ('../htdocs/Book.php');
-require_once ('../lib/RedBeanPHP/rb.php');
+//require_once ('../lib/RedBeanPHP/rb.php');
 
 
 /**
- * @covers Email
+ * @covers Book
  */
 final class Book_test extends \PHPUnit_Framework_TestCase {
 
-    public function test_validate_data() {
+   /* public function test_validate_data() {
         // phpunit Book_test.php
         $book_test = new Book();
         $return_values_scenario_one = $book_test->validate_data('', "title", "author", "publisher", 1999);
@@ -145,15 +145,15 @@ final class Book_test extends \PHPUnit_Framework_TestCase {
 
         $this->assertEquals('title minimum length is four , less cannot be accepted  <br>', $return_values_scenario_Twenty_six);
  // author cases start from here J. K. Rowling
-        
+
                $return_values_scenario_Twenty_seven = $book_test->validate_data("978-3-16-1", "title is hary potter", " J. K. Rowling ", "publisher is publisher",2016);
 
         $this->assertEquals('', $return_values_scenario_Twenty_seven);
- 
+
                $return_values_scenario_Twenty_eight = $book_test->validate_data("", "title is hary potter", " J. K. Rowling ", "publisher is publisher",2016);
 
         $this->assertEquals('ISBAN number is empty ! <br>', $return_values_scenario_Twenty_eight);
-        
+
          $return_values_scenario_Twenty_nine = $book_test->validate_data("", "", " J. K. Rowling ", "publisher is publisher",2016);
 
         $this->assertEquals('ISBAN number is empty ! <br>title minimum length is four , less cannot be accepted  <br>', $return_values_scenario_Twenty_nine);
@@ -162,26 +162,27 @@ final class Book_test extends \PHPUnit_Framework_TestCase {
 
         $this->assertEquals('title minimum length is four , less cannot be accepted  <br>', $return_values_scenario_Thirty);
 
-        
-        
+
+
           $return_values_scenario_Thirty_one = $book_test->validate_data("978-3-16-1", "title is something you should see it first", " J. K. Rowling ", "",2016);
 
         $this->assertEquals('publisher minimum length is four , less cannot be accepted  <br>', $return_values_scenario_Thirty_one);
-        
+
           $return_values_scenario_Thirty_two = $book_test->validate_data("978-3-16-1", "title is something you should see it first", " J. K. Rowling ", "publisher is nora",0);
 
         $this->assertEquals('publication_year is empty !   <br>', $return_values_scenario_Thirty_two);
-        
-        
+
+
 // phpunit Book_test.php
     }
+*/
 
-    
     public function test_save_book_data(){
-        
-        // save new object of the object allrady in the database delete it and make a new one .. and test if is save or not 
-        
-     $ISBN="1-33-44-55"; 
+
+        // save new object of the object allrady in the database delete it and make a new one .. and test if is save or not
+ $expected = array('.');
+ $actul=array(',');
+     $ISBN="1-33-44-55";
      $title="Freedom of the City of London";
      $author="J. K. Rowling";
      $publisher="pseudonym Robert Galbraith";
@@ -189,34 +190,53 @@ final class Book_test extends \PHPUnit_Framework_TestCase {
        $book=new Book();
         $saved = R::getAll("SELECT * FROM books where ISBN='$ISBN'  and title='$title' and author='$author'  and publisher='$publisher' and publication_year=".$publication_year );
        // echo $saved['id'];
-        
+
              foreach ($saved as $elm) {
                echo "id is". $id = $elm['id'];
             }
         if(count($saved)>0){
-        
-           
+
+
             // take isbn and delete the object ..
         $var= R::exec("DELETE FROM `books` WHERE ISBN='$ISBN'" );
         echo "var".$var;
          if($var){
 
-             
+
              $this->test_save_book_data();
-             
-        
+
+
          }
-         
+
         }else {
          // store the object again and test it , using select get all , the line number 181 just in case the book data saved allredy ..
-        // INSERT INTO `books`(`id`, `ISBN`, `title`, `author`, `publisher`, `publication_year`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6]) 
+        // INSERT INTO `books`(`id`, `ISBN`, `title`, `author`, `publisher`, `publication_year`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6])
           $num=   $book->save($ISBN, $title, $author, $publisher, $publication_year);
-          echo $num;
+          if($num){
+        $data_after_saving = R::getAll("SELECT * FROM books where ISBN='$ISBN'  and title='$title' and author='$author'  and publisher='$publisher' and publication_year=".$publication_year );
+
+        foreach ($data_after_saving as $data) {
+        //   $ISBN=$data['ISBN'];
+           // $title=$data['title'];
+        //     $author=$data['author'];
+           //   $publisher=$data['publisher'];
+            //   $publication_year=$data['publication_year'];
+               $actul=array($data['ISBN'],$data['title'], $data['author'],$data['publisher'],$data['publication_year']);
         }
-        
-        
+        if($data_after_saving){
+            
+       // $this->assertContains($ISBN, [$ISBN,$title,$author,$publisher,$publication_year]);
+       $expected = array("$ISBN", "$title", "$author","$publisher",$publication_year);
+
+        $this->assertSame(array_diff($expected, $actul), array_diff($actul, $expected));
+
+        } }
+        }
+
+
     }
-    
+// phpunit Book_test.php
+
 }
 
 
